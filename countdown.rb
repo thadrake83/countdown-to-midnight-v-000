07b43e6639b02eb58146246b4ddc6b8ecb1e7817
Drake Rentrop <drakerentrop@gmail.com>
2018-10-08 18:26:09 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -4,9 +4,8 @@ def countdown(number)
   while number > 0
     puts "#{number} SECOND(S)!"
     number -= 1
-    if number == 0
-      puts "HAPPY NEW YEAR!"
-    end
-
+end
+if number == 0
+  puts "HAPPY NEW YEAR!"
 end
 end
